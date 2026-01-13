cd0a161a838b31a194a021e6244fac39cc46ee63
manoj.kr <manojranganathanofficial@gmail.com>
2026-01-13 23:41:25 +0530

    browser headless is activated

--- src/main/java/com/utilities/BrowserConfig.java
+++ src/main/java/com/utilities/BrowserConfig.java
@@ -17,7 +17,7 @@ public class BrowserConfig {
 			WebDriverManager.chromedriver().setup();
 
 			ChromeOptions options = new ChromeOptions();
-			//options.addArguments("--headless=new");
+			options.addArguments("--headless=new");
 			options.addArguments("--start-maximized");
 			options.addArguments("--window-size=1920,1080");
 			options.addArguments("--disable-gpu");
